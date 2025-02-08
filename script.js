7e66788fbc0fc672caa0d065cb6087655356e945
tin22sh <133508417+tin22sh@users.noreply.github.com>
2025-02-08 23:23:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let isGenerating = false;
 
 const API_CONFIG = {
     'workers-api': {
-        url: 'https://1220.tts-api.zwei.de.eu.org/tts',
-        authToken: 'your-key'
+        url: 'https://ttsapi.yort.de/tts',
+        authToken: 'alsjfjfal78794'        
+        // url: 'https://1220.tts-api.zwei.de.eu.org/tts',
+        // authToken: 'your-key'
     },
     'deno-api': {
         url: 'https://deno-tts.api.zwei.de.eu.org/tts'
