0ae6d9224ccc351358b54d9331b9dffb7691f462
tin22sh <133508417+tin22sh@users.noreply.github.com>
2025-02-09 12:26:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ let isGenerating = false;
 
 const API_CONFIG = {
     'workers-api': {
-        url: 'https://voice-api-worker.391111.xyz/tts',
-        authToken: 'your-key'
+        url: 'https://voice-api-worker.391111.xyz/tts'
         // url: 'https://1220.tts-api.zwei.de.eu.org/tts',
         // authToken: 'your-key'
     },
